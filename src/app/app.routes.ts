860c419be4f231aff9d7bb57e8cb012e2b811ed3
Oleg Vasilev <mallocprof@gmail.com>
2025-06-15 20:13:40 +0700

    Refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@ import { Routes } from '@angular/router';
 import { RouteTableComponent } from './pages/route-table-component/route-table-component';
 
 export const routes: Routes = [
-	{ path: '', component: RouteTableComponent },
+	{ path: 'route-table', component: RouteTableComponent },
+	{ path: '', redirectTo: '/route-table', pathMatch: 'full' },
 	{ path: '**', loadComponent: () =>
 			import('./pages/no-found-component/no-found-component')
 				.then(c => c.NoFoundComponent)
